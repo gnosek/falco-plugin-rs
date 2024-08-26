3a6a1b5b0049018ef7ec4c8e667e4472a8466d05
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2024-08-26 23:57:59 +0200

    new(plugin): optionally thread-safe tables

--- falco_plugin/src/plugin/exported_tables/ref_shared.rs
+++ falco_plugin/src/plugin/exported_tables/ref_shared.rs
@@ -1,19 +1,24 @@
-use refcell_lock_api::CellRwLock;
 use std::sync::Arc;
 
+#[cfg(feature = "thread-safe-tables")]
+use parking_lot::RawRwLock as LockImpl;
+
+#[cfg(not(feature = "thread-safe-tables"))]
+use refcell_lock_api::raw::CellRwLock as LockImpl;
+
 /// like `RefCell<T>`
-pub type RefCounted<T> = CellRwLock<T>;
+pub type RefCounted<T> = lock_api::RwLock<LockImpl, T>;
 
 /// like `Rc<RefCell<T>>`
-pub type RefShared<T> = Arc<CellRwLock<T>>;
+pub type RefShared<T> = Arc<RefCounted<T>>;
 
 /// like `Rc<RefCell<T>> + RefMut<T>`
-pub type RefGuard<T> = lock_api::ArcRwLockWriteGuard<refcell_lock_api::raw::CellRwLock, T>;
+pub type RefGuard<T> = lock_api::ArcRwLockWriteGuard<LockImpl, T>;
 
 pub fn new_shared_ref<T>(inner: T) -> RefShared<T> {
-    Arc::new(CellRwLock::new(inner))
+    Arc::new(RefCounted::new(inner))
 }
 
 pub fn new_counted_ref<T>(inner: T) -> RefCounted<T> {
-    CellRwLock::new(inner)
+    RefCounted::new(inner)
 }
