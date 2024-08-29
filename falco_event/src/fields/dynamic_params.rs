8fa3bfef7c1b863ec46e5a7d4b8e75585180cc63
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2024-08-29 12:02:02 +0200

    chore(event): reformat generated files

--- falco_event/src/fields/dynamic_params.rs
+++ falco_event/src/fields/dynamic_params.rs
@@ -2,20 +2,20 @@ use falco_event_derive::dynamic_params;
 
 dynamic_params! {
 const struct ppm_param_info sockopt_dynamic_param[PPM_SOCKOPT_IDX_MAX] = {
-	[PPM_SOCKOPT_IDX_UNKNOWN] = {{0}, PT_BYTEBUF, PF_HEX, 0, 0},
-	[PPM_SOCKOPT_IDX_ERRNO] = {{0}, PT_ERRNO, PF_DEC, 0, 0},
-	[PPM_SOCKOPT_IDX_UINT32] = {{0}, PT_UINT32, PF_DEC, 0, 0},
-	[PPM_SOCKOPT_IDX_UINT64] = {{0}, PT_UINT64, PF_DEC, 0, 0},
-	[PPM_SOCKOPT_IDX_TIMEVAL] = {{0}, PT_RELTIME, PF_DEC, 0, 0},
+    [PPM_SOCKOPT_IDX_UNKNOWN] = {{0}, PT_BYTEBUF, PF_HEX, 0, 0},
+    [PPM_SOCKOPT_IDX_ERRNO] = {{0}, PT_ERRNO, PF_DEC, 0, 0},
+    [PPM_SOCKOPT_IDX_UINT32] = {{0}, PT_UINT32, PF_DEC, 0, 0},
+    [PPM_SOCKOPT_IDX_UINT64] = {{0}, PT_UINT64, PF_DEC, 0, 0},
+    [PPM_SOCKOPT_IDX_TIMEVAL] = {{0}, PT_RELTIME, PF_DEC, 0, 0},
 };
 
 const struct ppm_param_info ptrace_dynamic_param[PPM_PTRACE_IDX_MAX] = {
-	[PPM_PTRACE_IDX_UINT64] = {{0}, PT_UINT64, PF_HEX, 0, 0},
-	[PPM_PTRACE_IDX_SIGTYPE] = {{0}, PT_SIGTYPE, PF_DEC, 0, 0},
+    [PPM_PTRACE_IDX_UINT64] = {{0}, PT_UINT64, PF_HEX, 0, 0},
+    [PPM_PTRACE_IDX_SIGTYPE] = {{0}, PT_SIGTYPE, PF_DEC, 0, 0},
 };
 
 const struct ppm_param_info bpf_dynamic_param[PPM_BPF_IDX_MAX] = {
-	[PPM_BPF_IDX_FD] = {{0}, PT_FD, PF_DEC, 0, 0},
-	[PPM_BPF_IDX_RES] = {{0}, PT_ERRNO, PF_DEC, 0, 0},
+    [PPM_BPF_IDX_FD] = {{0}, PT_FD, PF_DEC, 0, 0},
+    [PPM_BPF_IDX_RES] = {{0}, PT_ERRNO, PF_DEC, 0, 0},
 };
 }
