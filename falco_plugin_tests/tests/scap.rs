97993ae3c35f0b471915106d7e38e456ab9ca732
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2024-09-12 16:12:33 +0200

    new(tests): tests using scap files

---
+++ falco_plugin_tests/tests/scap.rs
@@ -0,0 +1,129 @@
+use falco_plugin::anyhow;
+use falco_plugin::anyhow::{Context, Error};
+use falco_plugin::base::Plugin;
+use falco_plugin::event::events::types::EventType;
+use falco_plugin::extract::EventInput;
+use falco_plugin::parse::{ParseInput, ParsePlugin};
+use falco_plugin::static_plugin;
+use falco_plugin::tables::TablesInput;
+use std::ffi::CStr;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+// we cant really get at the plugin state once we hand it off
+// to the framework, so add some global state so we see
+// what it's doing
+thread_local! {
+static GOT_EVENTS: AtomicUsize = const { AtomicUsize::new(0) };
+static PARSED_EVENTS: AtomicUsize = const { AtomicUsize::new(0) };
+}
+
+struct DummyPlugin {
+    event_num: usize,
+}
+
+impl Plugin for DummyPlugin {
+    const NAME: &'static CStr = c"dummy";
+    const PLUGIN_VERSION: &'static CStr = c"0.0.0";
+    const DESCRIPTION: &'static CStr = c"test plugin";
+    const CONTACT: &'static CStr = c"rust@localdomain.pl";
+    type ConfigType = ();
+
+    fn new(_input: Option<&TablesInput>, _config: Self::ConfigType) -> Result<Self, Error> {
+        GOT_EVENTS.with(|ge| ge.store(0, Ordering::Relaxed));
+        PARSED_EVENTS.with(|pe| pe.store(0, Ordering::Relaxed));
+        Ok(Self { event_num: 0 })
+    }
+}
+
+impl ParsePlugin for DummyPlugin {
+    const EVENT_TYPES: &'static [EventType] = &[];
+    const EVENT_SOURCES: &'static [&'static str] = &["syscall"];
+
+    fn parse_event(&mut self, event: &EventInput, _parse_input: &ParseInput) -> anyhow::Result<()> {
+        GOT_EVENTS.with(|ge| ge.fetch_add(1, Ordering::Relaxed));
+        let event = event
+            .event()
+            .context(format!("loading raw event {})", self.event_num))?;
+        event
+            .load_any()
+            .context(format!("parsing event #{} {event:?}", self.event_num))?;
+
+        self.event_num += 1;
+        PARSED_EVENTS.with(|pe| pe.fetch_add(1, Ordering::Relaxed));
+
+        Ok(())
+    }
+}
+
+static_plugin!(PARSE_API = DummyPlugin);
+
+#[cfg(test)]
+mod tests {
+    use falco_plugin_tests::{
+        init_plugin, CaptureNotStarted, CaptureStarted, ScapStatus, SinspTestDriver,
+    };
+    use std::ffi::CString;
+    use std::os::unix::ffi::OsStrExt;
+    use std::path::PathBuf;
+    use std::sync::atomic::Ordering;
+
+    fn open_capture_file(
+        driver: SinspTestDriver<CaptureNotStarted>,
+    ) -> anyhow::Result<SinspTestDriver<CaptureStarted>> {
+        let manifest_dir = env!("CARGO_MANIFEST_DIR");
+        let scap_file = PathBuf::from(manifest_dir).join("tests/scap/kexec_x86.scap");
+        let scap_file = CString::new(scap_file.as_os_str().as_bytes())?;
+
+        driver.load_capture_file(scap_file.as_c_str())
+    }
+
+    #[test]
+    fn test_capture_open() {
+        let (driver, _plugin) = init_plugin(super::PARSE_API, c"").unwrap();
+        open_capture_file(driver).unwrap();
+    }
+
+    #[test]
+    fn test_count_events() {
+        let (driver, _plugin) = init_plugin(super::PARSE_API, c"").unwrap();
+        let mut driver = open_capture_file(driver).unwrap();
+
+        let mut counter = 0;
+        loop {
+            match driver.next_event() {
+                Ok(_) => {
+                    counter += 1;
+                }
+                Err(ScapStatus::Eof) => break,
+                Err(e) => panic!("{:?}", e),
+            }
+        }
+
+        assert_eq!(counter, 523412);
+    }
+
+    #[test]
+    fn test_with_plugin() {
+        let (driver, _plugin) = init_plugin(super::PARSE_API, c"").unwrap();
+        let mut driver = open_capture_file(driver).unwrap();
+
+        let mut counter = 0;
+        loop {
+            match driver.next_event() {
+                Ok(_) => {
+                    counter += 1;
+                }
+                Err(ScapStatus::Eof) => break,
+                Err(e) => panic!("{:?}", e),
+            }
+        }
+
+        assert_eq!(counter, 523412);
+
+        // for whatever reason, we're not getting all the events in the parse plugin,
+        // but whatever we get, we should parse without errors
+        let got_events = super::GOT_EVENTS.with(|e| e.load(Ordering::Relaxed));
+        let parsed_events = super::PARSED_EVENTS.with(|e| e.load(Ordering::Relaxed));
+        assert_eq!(got_events, parsed_events);
+    }
+}
---
+++ falco_plugin_tests/tests/scap_import_table.rs
@@ -0,0 +1,185 @@
+use falco_plugin::anyhow;
+use falco_plugin::anyhow::{Context, Error};
+use falco_plugin::base::Plugin;
+use falco_plugin::event::events::types::{EventType, PPME_SYSCALL_READ_E};
+use falco_plugin::event::events::Event;
+use falco_plugin::event::fields::types::PT_FD;
+use falco_plugin::extract::EventInput;
+use falco_plugin::parse::{ParseInput, ParsePlugin};
+use falco_plugin::static_plugin;
+use falco_plugin::tables::import::{Entry, Field, Table, TableMetadata};
+use falco_plugin::tables::TablesInput;
+use std::collections::BTreeMap;
+use std::ffi::CStr;
+use std::rc::Rc;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+type Fd = Entry<Rc<FdMetadata>>;
+type FdTable = Table<i64, Fd>;
+
+#[derive(TableMetadata)]
+#[entry_type(Fd)]
+struct FdMetadata {
+    fd: Field<i64, Fd>,
+
+    #[name(c"type")]
+    fd_type: Field<u8, Fd>,
+}
+
+type Thread = Entry<Rc<ThreadMetadata>>;
+type ThreadTable = Table<i64, Thread>;
+
+#[derive(TableMetadata)]
+#[entry_type(Thread)]
+struct ThreadMetadata {
+    comm: Field<CStr, Thread>,
+    file_descriptors: Field<FdTable, Thread>,
+
+    #[custom]
+    num_events: Field<u64, Thread>,
+}
+
+thread_local! {
+static TEST_DONE: AtomicBool = AtomicBool::new(false);
+}
+
+struct DummyPlugin {
+    threads: ThreadTable,
+    event_num: usize,
+}
+
+impl Plugin for DummyPlugin {
+    const NAME: &'static CStr = c"dummy";
+    const PLUGIN_VERSION: &'static CStr = c"0.0.0";
+    const DESCRIPTION: &'static CStr = c"test plugin";
+    const CONTACT: &'static CStr = c"rust@localdomain.pl";
+    type ConfigType = ();
+
+    fn new(input: Option<&TablesInput>, _config: Self::ConfigType) -> Result<Self, Error> {
+        TEST_DONE.with(|flag| flag.store(false, Ordering::Relaxed));
+
+        let Some(input) = input else {
+            anyhow::bail!("Did not get tables input")
+        };
+
+        let threads = input.get_table(c"threads")?;
+
+        Ok(Self {
+            threads,
+            event_num: 0,
+        })
+    }
+}
+
+impl ParsePlugin for DummyPlugin {
+    const EVENT_TYPES: &'static [EventType] = &[];
+    const EVENT_SOURCES: &'static [&'static str] = &["syscall"];
+
+    fn parse_event(&mut self, event: &EventInput, parse_input: &ParseInput) -> anyhow::Result<()> {
+        self.event_num += 1;
+
+        // matching against a random(ish) event in kexec_x86.scap
+        if self.event_num == 1452 {
+            let event = event
+                .event()
+                .context(format!("loading raw event {})", self.event_num))?;
+            let event: Event<PPME_SYSCALL_READ_E> = event
+                .load()
+                .context(format!("parsing event #{} {event:?}", self.event_num))?;
+
+            let Some(PT_FD(event_fd)) = event.params.fd else {
+                anyhow::bail!("event did not have the fd param set");
+            };
+
+            if event_fd != 16 {
+                anyhow::bail!("event param mismatch, expected fd=16, got {}", event_fd);
+            }
+
+            let r = &parse_input.reader;
+
+            let tid = event.metadata.tid;
+            let thread = self.threads.get_entry(r, &tid)?;
+
+            let comm = thread.get_comm(r)?;
+            if comm.to_bytes() != b"node" {
+                anyhow::bail!("comm mismatch, expected \"node\", got {:?}", comm);
+            }
+
+            let event_fd_entry = thread.get_file_descriptors_by_key(r, &event_fd)?;
+            let fd_type = event_fd_entry.get_fd_type(r)?;
+            if fd_type != 9 {
+                anyhow::bail!("fd type mismatch in direct lookup, got {}", fd_type);
+            }
+
+            let mut fd_map = BTreeMap::new();
+            let fds = thread.get_file_descriptors(&parse_input.reader)?;
+            fds.iter_entries_mut(&parse_input.reader, |fd| {
+                let Ok(fd_num) = fd.get_fd(&parse_input.reader) else {
+                    return true;
+                };
+                let Ok(fd_type) = fd.get_fd_type(&parse_input.reader) else {
+                    return true;
+                };
+
+                fd_map.insert(fd_num, fd_type);
+                true
+            });
+
+            if fd_map.len() != 33 {
+                anyhow::bail!("fd table length mismatch, got {}", fd_map.len());
+            }
+
+            let fd_type = fd_map.get(&event_fd);
+            if fd_type != Some(&9) {
+                anyhow::bail!(
+                    "fd type mismatch in iter_entries_mut map, got {:?}",
+                    fd_type
+                )
+            }
+
+            TEST_DONE.with(|flag| flag.store(true, Ordering::Relaxed));
+        }
+
+        Ok(())
+    }
+}
+
+static_plugin!(PARSE_API = DummyPlugin);
+
+#[cfg(test)]
+mod tests {
+    use crate::TEST_DONE;
+    use falco_plugin_tests::{
+        init_plugin, CaptureNotStarted, CaptureStarted, ScapStatus, SinspTestDriver,
+    };
+    use std::ffi::CString;
+    use std::os::unix::ffi::OsStrExt;
+    use std::path::PathBuf;
+    use std::sync::atomic::Ordering;
+
+    fn open_capture_file(
+        driver: SinspTestDriver<CaptureNotStarted>,
+    ) -> anyhow::Result<SinspTestDriver<CaptureStarted>> {
+        let manifest_dir = env!("CARGO_MANIFEST_DIR");
+        let scap_file = PathBuf::from(manifest_dir).join("tests/scap/kexec_x86.scap");
+        let scap_file = CString::new(scap_file.as_os_str().as_bytes())?;
+
+        driver.load_capture_file(scap_file.as_c_str())
+    }
+
+    #[test]
+    fn test_with_plugin() {
+        let (driver, _plugin) = init_plugin(super::PARSE_API, c"").unwrap();
+        let mut driver = open_capture_file(driver).unwrap();
+
+        loop {
+            match driver.next_event() {
+                Ok(_) => continue,
+                Err(ScapStatus::Eof) => break,
+                Err(e) => panic!("{:?}", e),
+            }
+        }
+
+        assert!(TEST_DONE.with(|flag| flag.load(Ordering::Relaxed)));
+    }
+}
