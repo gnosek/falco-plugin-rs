20a029e28cdf5fcd17d10072be894f0adaebca69
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-03-30 01:10:31 +0100

    cleanup(event): format PT_IPV*NET using Debug
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- falco_event/src/types/net/ipv6net.rs
+++ falco_event/src/types/net/ipv6net.rs
@@ -1,7 +1,7 @@
 use crate::event_derive::{FromBytes, FromBytesResult, ToBytes};
 use crate::format::FormatType;
 use crate::types::format::Format;
-use std::fmt::Formatter;
+use std::fmt::{Debug, Formatter};
 use std::io::Write;
 use std::net::Ipv6Addr;
 
@@ -10,7 +10,7 @@ use std::net::Ipv6Addr;
 /// This is a wrapper around [Ipv6Addr] that makes it a distinct type, suitable for storing
 /// IPv6 subnets.
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub struct Ipv6Net(pub Ipv6Addr);
 
 impl FromBytes<'_> for Ipv6Net {
@@ -33,9 +33,15 @@ impl ToBytes for Ipv6Net {
     }
 }
 
+impl Debug for Ipv6Net {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(&self.0, f)
+    }
+}
+
 impl Format for Ipv6Net {
     fn format(&self, _format_type: FormatType, fmt: &mut Formatter) -> std::fmt::Result {
-        write!(fmt, "{}", self.0)
+        Debug::fmt(self, fmt)
     }
 }
 
