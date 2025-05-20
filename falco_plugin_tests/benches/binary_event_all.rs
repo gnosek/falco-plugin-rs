2aef357e0e06d2be9196ad749eb0c07ee578af32
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-05-21 00:14:15 +0200

    new(tests): bulk parsing/dumping benchmark
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

---
+++ falco_plugin_tests/benches/binary_event_all.rs
@@ -0,0 +1,55 @@
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use falco_plugin::event::events::types::AnyEvent;
+use falco_plugin::event::events::{Event, EventToBytes, RawEvent};
+use std::hint::black_box;
+use std::path::PathBuf;
+
+fn load(events: &[u8]) -> Vec<Event<AnyEvent>> {
+    RawEvent::scan(events)
+        .map(|event| event.unwrap().load_any().unwrap())
+        .collect()
+}
+
+fn test_parse(events: &[u8]) {
+    RawEvent::scan(events)
+        .map(|event| event.unwrap().load_any().unwrap())
+        .for_each(|event| {
+            black_box(event);
+        });
+}
+
+fn test_dump(events: &[Event<AnyEvent>], out: &mut Vec<u8>) {
+    out.clear();
+    for event in events {
+        event.write(&mut *out).unwrap();
+    }
+}
+
+fn bench_parse(c: &mut Criterion) {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    let events_file = PathBuf::from(manifest_dir).join("tests/scap/kexec_x86.raw");
+    let events = std::fs::read(&events_file).unwrap();
+
+    let mut g = c.benchmark_group("event_parse_all");
+    g.throughput(Throughput::Bytes(events.len() as u64));
+    g.bench_function("event_parse_all", |b| {
+        b.iter(|| test_parse(black_box(&events)))
+    });
+}
+
+fn bench_dump(c: &mut Criterion) {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    let events_file = PathBuf::from(manifest_dir).join("tests/scap/kexec_x86.raw");
+    let events = std::fs::read(&events_file).unwrap();
+    let events = load(&events);
+    let mut out = Vec::new();
+
+    let mut g = c.benchmark_group("event_dump_all");
+    g.throughput(Throughput::Elements(events.len() as u64));
+    g.bench_function("event_dump_all", |b| {
+        b.iter(|| test_dump(black_box(events.as_slice()), &mut out))
+    });
+}
+
+criterion_group!(benches, bench_parse, bench_dump);
+criterion_main!(benches);
---
+++ falco_plugin_tests/src/bin/dump_raw_events.rs
@@ -0,0 +1,69 @@
+use anyhow::Error;
+use falco_plugin::base::Plugin;
+use falco_plugin::event::events::EventToBytes;
+use falco_plugin::parse::{EventInput, ParseInput, ParsePlugin};
+use falco_plugin::static_plugin;
+use falco_plugin::tables::TablesInput;
+use std::ffi::CStr;
+use std::fs::File;
+
+struct DumperPlugin(File);
+
+impl Plugin for DumperPlugin {
+    const NAME: &'static CStr = c"dumper_test_plugin";
+    const PLUGIN_VERSION: &'static CStr = c"0.0.0";
+    const DESCRIPTION: &'static CStr = c"dump all events as raw bytes to an output file";
+    const CONTACT: &'static CStr = c"https://github.com/falcosecurity/plugin-sdk-rs";
+    type ConfigType = String;
+
+    fn new(_input: Option<&TablesInput>, config: Self::ConfigType) -> Result<Self, Error> {
+        let output = std::fs::File::create(config)?;
+        Ok(DumperPlugin(output))
+    }
+}
+
+impl ParsePlugin for DumperPlugin {
+    const EVENT_TYPES: &'static [falco_plugin::event::events::types::EventType] = &[];
+    const EVENT_SOURCES: &'static [&'static str] = &[];
+
+    fn parse_event(&mut self, event: &EventInput, _parse_input: &ParseInput) -> anyhow::Result<()> {
+        let event = event.event()?;
+        Ok(event.write(&mut self.0)?)
+    }
+}
+
+static_plugin!(DUMPER_PLUGIN = DumperPlugin);
+
+#[cfg(not(have_libsinsp))]
+fn main() {
+    panic!("libsinsp not available");
+}
+
+#[cfg(have_libsinsp)]
+fn main() {
+    use falco_plugin_tests::CapturingTestDriver;
+    use falco_plugin_tests::SavefileTestDriver;
+    use falco_plugin_tests::ScapStatus;
+    use falco_plugin_tests::TestDriver;
+    use std::ffi::CString;
+
+    let scap_path = std::env::args().nth(1).unwrap();
+    let scap_path = CString::new(scap_path).unwrap();
+
+    let output_path = std::env::args().nth(2).unwrap();
+    let output_path = CString::new(output_path).unwrap();
+
+    let mut driver = falco_plugin_tests::ffi::Driver::new().unwrap();
+    driver
+        .register_plugin(&DUMPER_PLUGIN, output_path.as_c_str())
+        .unwrap();
+    let mut driver = driver.load_capture_file(scap_path.as_c_str()).unwrap();
+
+    loop {
+        match driver.next_event() {
+            Ok(_) => continue,
+            Err(ScapStatus::Eof) => break,
+            Err(e) => panic!("{:?}", e),
+        }
+    }
+}
