9f4ff8d95fb7c40a5c9672e982cbf9943d1ca9f2
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-08-19 07:50:06 +0200

    chore(tests): use CountdownPlugin in source_batch
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- falco_plugin_tests/tests/source_batch.rs
+++ falco_plugin_tests/tests/source_batch.rs
@@ -1,139 +1,45 @@
-use falco_plugin::anyhow::Error;
-use falco_plugin::base::{Metric, MetricLabel, MetricType, MetricValue, Plugin};
-use falco_plugin::source::{
-    EventBatch, EventInput, PluginEvent, SourcePlugin, SourcePluginInstance,
-};
-use falco_plugin::strings::CStringWriter;
-use falco_plugin::tables::TablesInput;
-use falco_plugin::{anyhow, static_plugin, FailureReason};
-use std::ffi::{CStr, CString};
-use std::io::Write;
-
-struct DummyPlugin {
-    num_batches: usize,
-    batch_count: MetricLabel,
-}
-
-impl Plugin for DummyPlugin {
-    const NAME: &'static CStr = c"dummy";
-    const PLUGIN_VERSION: &'static CStr = c"0.0.0";
-    const DESCRIPTION: &'static CStr = c"test plugin";
-    const CONTACT: &'static CStr = c"rust@localdomain.pl";
-    type ConfigType = ();
-
-    fn new(_input: Option<&TablesInput>, _config: Self::ConfigType) -> Result<Self, Error> {
-        Ok(Self {
-            num_batches: 0,
-            batch_count: MetricLabel::new(c"next_batch_call_count", MetricType::Monotonic),
-        })
-    }
-
-    fn get_metrics(&mut self) -> impl IntoIterator<Item = Metric> {
-        [self
-            .batch_count
-            .with_value(MetricValue::U64(self.num_batches as u64))]
-    }
-}
-
-struct DummyPluginInstance(Option<usize>);
-
-impl SourcePluginInstance for DummyPluginInstance {
-    type Plugin = DummyPlugin;
-
-    fn next_batch(
-        &mut self,
-        plugin: &mut Self::Plugin,
-        batch: &mut EventBatch,
-    ) -> Result<(), Error> {
-        plugin.num_batches += 1;
-        if let Some(mut num_events) = self.0.take() {
-            while num_events > 0 {
-                num_events -= 1;
-                let event = format!("{num_events} events remaining");
-                let event = Self::plugin_event(event.as_bytes());
-                batch.add(event)?;
-            }
-            Ok(())
-        } else {
-            Err(anyhow::anyhow!("all events produced").context(FailureReason::Eof))
-        }
-    }
-}
-
-impl SourcePlugin for DummyPlugin {
-    type Instance = DummyPluginInstance;
-    const EVENT_SOURCE: &'static CStr = c"dummy";
-    const PLUGIN_ID: u32 = 1111;
-
-    fn open(&mut self, _params: Option<&str>) -> Result<Self::Instance, Error> {
-        Ok(DummyPluginInstance(Some(4)))
-    }
-
-    fn event_to_string(&mut self, event: &EventInput) -> Result<CString, Error> {
-        let event = event.event()?;
-        let plugin_event = event.load::<PluginEvent>()?;
-        let mut writer = CStringWriter::default();
-        write!(
-            writer,
-            "{}",
-            plugin_event
-                .params
-                .event_data
-                .map(|e| String::from_utf8_lossy(e))
-                .unwrap_or_default()
-        )?;
-        Ok(writer.into_cstring())
-    }
-}
-
-static_plugin!(DUMMY_PLUGIN_API = DummyPlugin);
-
 #[cfg(test)]
 mod tests {
     use falco_plugin::base::Plugin;
+    use falco_plugin_tests::plugin_collection::source::countdown::{
+        check_metrics, CountdownPlugin, COUNTDOWN_PLUGIN_API,
+    };
     use falco_plugin_tests::{
         init_plugin, instantiate_tests, CapturingTestDriver, PlatformData, ScapStatus, TestDriver,
     };
 
-    fn check_metrics<C: CapturingTestDriver>(driver: &mut C, n: usize) {
-        let metrics = driver.get_metrics().unwrap();
-        let mut metrics = metrics.iter();
-
-        let m = metrics.next().unwrap();
-        assert_eq!(m.name, "dummy.next_batch_call_count");
-        assert_eq!(m.value, n as u64);
-
-        assert!(metrics.next().is_none());
-    }
-
     fn test_dummy_next<D: TestDriver>() {
-        let (driver, _plugin) = init_plugin::<D>(&super::DUMMY_PLUGIN_API, c"").unwrap();
+        let (driver, _plugin) = init_plugin::<D>(
+            &COUNTDOWN_PLUGIN_API,
+            cr#"{"remaining": 4, "batch_size": 4}"#,
+        )
+        .unwrap();
         let mut driver = driver
-            .start_capture(super::DummyPlugin::NAME, c"", PlatformData::Disabled)
+            .start_capture(CountdownPlugin::NAME, c"", PlatformData::Disabled)
             .unwrap();
 
         assert_eq!(
             driver.next_event_as_str().unwrap().unwrap(),
             "3 events remaining"
         );
-        check_metrics(&mut driver, 1);
+        check_metrics(&mut driver, 1, 4);
         assert_eq!(
             driver.next_event_as_str().unwrap().unwrap(),
             "2 events remaining"
         );
-        check_metrics(&mut driver, 1);
+        check_metrics(&mut driver, 1, 4);
         assert_eq!(
             driver.next_event_as_str().unwrap().unwrap(),
             "1 events remaining"
         );
-        check_metrics(&mut driver, 1);
+        check_metrics(&mut driver, 1, 4);
         assert_eq!(
             driver.next_event_as_str().unwrap().unwrap(),
             "0 events remaining"
         );
 
         let event = driver.next_event();
-        check_metrics(&mut driver, 2);
+        check_metrics(&mut driver, 2, 4);
         assert!(matches!(event, Err(ScapStatus::Eof)))
     }
 
