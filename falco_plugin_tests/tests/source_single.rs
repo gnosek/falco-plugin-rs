e16d8c6888909bbcc1dad661c52b399a3ecff075
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-08-19 07:49:24 +0200

    chore(tests): flatten source_single.rs
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- falco_plugin_tests/tests/source_single.rs
+++ falco_plugin_tests/tests/source_single.rs
@@ -1,45 +1,42 @@
-#[cfg(test)]
-mod tests {
-    use falco_plugin::base::Plugin;
-    use falco_plugin_tests::plugin_collection::source::countdown::{
-        check_metrics, CountdownPlugin, COUNTDOWN_PLUGIN_API,
-    };
-    use falco_plugin_tests::{
-        init_plugin, instantiate_tests, CapturingTestDriver, PlatformData, ScapStatus, TestDriver,
-    };
+use falco_plugin::base::Plugin;
+use falco_plugin_tests::plugin_collection::source::countdown::{
+    check_metrics, CountdownPlugin, COUNTDOWN_PLUGIN_API,
+};
+use falco_plugin_tests::{
+    init_plugin, instantiate_tests, CapturingTestDriver, PlatformData, ScapStatus, TestDriver,
+};
 
-    fn test_dummy_next<D: TestDriver>() {
-        let (driver, _plugin) = init_plugin::<D>(
-            &COUNTDOWN_PLUGIN_API,
-            cr#"{"remaining": 3, "batch_size": 1}"#,
-        )
+fn test_dummy_next<D: TestDriver>() {
+    let (driver, _plugin) = init_plugin::<D>(
+        &COUNTDOWN_PLUGIN_API,
+        cr#"{"remaining": 3, "batch_size": 1}"#,
+    )
+    .unwrap();
+    let mut driver = driver
+        .start_capture(CountdownPlugin::NAME, c"", PlatformData::Disabled)
         .unwrap();
-        let mut driver = driver
-            .start_capture(CountdownPlugin::NAME, c"", PlatformData::Disabled)
-            .unwrap();
 
-        assert_eq!(
-            driver.next_event_as_str().unwrap().unwrap(),
-            "2 events remaining"
-        );
-        check_metrics(&mut driver, 1, 1);
+    assert_eq!(
+        driver.next_event_as_str().unwrap().unwrap(),
+        "2 events remaining"
+    );
+    check_metrics(&mut driver, 1, 1);
 
-        assert_eq!(
-            driver.next_event_as_str().unwrap().unwrap(),
-            "1 events remaining"
-        );
-        check_metrics(&mut driver, 2, 2);
+    assert_eq!(
+        driver.next_event_as_str().unwrap().unwrap(),
+        "1 events remaining"
+    );
+    check_metrics(&mut driver, 2, 2);
 
-        assert_eq!(
-            driver.next_event_as_str().unwrap().unwrap(),
-            "0 events remaining"
-        );
-        check_metrics(&mut driver, 3, 3);
+    assert_eq!(
+        driver.next_event_as_str().unwrap().unwrap(),
+        "0 events remaining"
+    );
+    check_metrics(&mut driver, 3, 3);
 
-        let event = driver.next_event();
-        check_metrics(&mut driver, 4, 3);
-        assert!(matches!(event, Err(ScapStatus::Eof)))
-    }
-
-    instantiate_tests!(test_dummy_next);
+    let event = driver.next_event();
+    check_metrics(&mut driver, 4, 3);
+    assert!(matches!(event, Err(ScapStatus::Eof)))
 }
+
+instantiate_tests!(test_dummy_next);
