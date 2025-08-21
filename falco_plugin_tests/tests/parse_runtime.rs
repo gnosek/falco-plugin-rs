c4e8b79fd786a6512e060c15199eb8214c3e0b9c
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-08-21 06:21:15 +0200

    chore(tests): better test name in tests/parse_runtime.rs
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- falco_plugin_tests/tests/parse_runtime.rs
+++ falco_plugin_tests/tests/parse_runtime.rs
@@ -8,7 +8,7 @@ use falco_plugin_tests::{
     init_plugin, instantiate_tests, CapturingTestDriver, PlatformData, ScapStatus, TestDriver,
 };
 
-fn test_dummy_next<D: TestDriver>() {
+fn test_parse_runtime<D: TestDriver>() {
     let (mut driver, _plugin) = init_plugin::<D>(
         &COUNTDOWN_PLUGIN_API,
         cr#"{"remaining": 4, "batch_size": 4}"#,
@@ -65,4 +65,4 @@ fn test_dummy_next<D: TestDriver>() {
     assert!(matches!(event, Err(ScapStatus::Eof)))
 }
 
-instantiate_tests!(test_dummy_next);
+instantiate_tests!(test_parse_runtime);
